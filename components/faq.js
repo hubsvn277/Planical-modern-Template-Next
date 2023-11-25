99605aa0bdb3c3ad617cdb50b9e85199b4bee309
HubsVN <146311806+hubsvn277@users.noreply.github.com>
2023-11-25 21:40:33 +0700

    Commit from TeleportHQ

---
+++ components/faq.js
@@ -0,0 +1,451 @@
+import React from 'react'
+
+import PropTypes from 'prop-types'
+
+const FAQ = (props) => {
+  return (
+    <>
+      <div className={`faq-accordion ${props.rootClassName} `}>
+        <div
+          data-role="accordion-container"
+          className="faq-element accordion-element"
+        >
+          <div className="faq-details">
+            <span className="faq-text">
+              What is sit amet, consectetur adipiscing elit, sed do?
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span data-role="accordion-content" className="faq-text01">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id.
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </div>
+          <div data-role="accordion-icon">
+            <svg viewBox="0 0 1024 1024" className="faq-icon">
+              <path d="M366 708l196-196-196-196 60-60 256 256-256 256z"></path>
+            </svg>
+          </div>
+        </div>
+        <div
+          data-role="accordion-container"
+          className="faq-element1 accordion-element"
+        >
+          <div className="faq-details1">
+            <span className="faq-text02">
+              We&apos;ll update amet, consectetur adipiscing elit until
+              you&apos;re 100% happy.
+            </span>
+            <span data-role="accordion-content" className="faq-text03">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id.
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </div>
+          <div data-role="accordion-icon">
+            <svg viewBox="0 0 1024 1024" className="faq-icon02">
+              <path d="M366 708l196-196-196-196 60-60 256 256-256 256z"></path>
+            </svg>
+          </div>
+        </div>
+        <div
+          data-role="accordion-container"
+          className="faq-element2 accordion-element"
+        >
+          <div className="faq-details2">
+            <span className="faq-text04">
+              Is it really sit amet, consectetur adipiscing elit, sed do?
+            </span>
+            <span data-role="accordion-content" className="faq-text05">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id.
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </div>
+          <div data-role="accordion-icon">
+            <svg viewBox="0 0 1024 1024" className="faq-icon04">
+              <path d="M366 708l196-196-196-196 60-60 256 256-256 256z"></path>
+            </svg>
+          </div>
+        </div>
+        <div
+          data-role="accordion-container"
+          className="faq-element3 accordion-element"
+        >
+          <div className="faq-details3">
+            <span className="faq-text06">
+              We work, what is sit amet, consectetur adipiscing elit, sed do
+              sint occaecat cupidatat non proident
+            </span>
+            <span data-role="accordion-content" className="faq-text07">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id.
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </div>
+          <div data-role="accordion-icon">
+            <svg viewBox="0 0 1024 1024" className="faq-icon06">
+              <path d="M366 708l196-196-196-196 60-60 256 256-256 256z"></path>
+            </svg>
+          </div>
+        </div>
+        <div
+          data-role="accordion-container"
+          className="faq-element4 accordion-element"
+        >
+          <div className="faq-details4">
+            <span className="faq-text08">
+              What is sit amet, consectetur adipiscing elit, sed do?
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <span data-role="accordion-content" className="faq-text09">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id.
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </div>
+          <div data-role="accordion-icon">
+            <svg viewBox="0 0 1024 1024" className="faq-icon08">
+              <path d="M366 708l196-196-196-196 60-60 256 256-256 256z"></path>
+            </svg>
+          </div>
+        </div>
+        <div
+          data-role="accordion-container"
+          className="faq-element5 accordion-element"
+        >
+          <div className="faq-details5">
+            <span className="faq-text10">
+              Amet, consectetur adipiscing elit mollit anim id est laborum.
+            </span>
+            <span data-role="accordion-content" className="faq-text11">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt mollit
+              anim id.
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </div>
+          <div data-role="accordion-icon">
+            <svg viewBox="0 0 1024 1024" className="faq-icon10">
+              <path d="M366 708l196-196-196-196 60-60 256 256-256 256z"></path>
+            </svg>
+          </div>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          .faq-accordion {
+            width: 100%;
+            display: flex;
+            position: relative;
+            align-items: center;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .faq-element {
+            width: 100%;
+            max-width: 800px;
+            padding-top: var(--dl-space-space-threeunits);
+            padding-bottom: var(--dl-space-space-threeunits);
+          }
+          .faq-details {
+            gap: var(--dl-space-space-oneandhalfunits);
+            width: 100%;
+            display: flex;
+            align-items: flex-start;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .faq-text {
+            color: rgb(255, 255, 255);
+            width: 100%;
+            font-size: 20px;
+            font-style: normal;
+            font-family: Poppins;
+            font-weight: 500;
+            line-height: 28px;
+          }
+          .faq-text01 {
+            color: rgb(204, 204, 204);
+            width: 100%;
+            font-family: Poppins;
+            line-height: 24px;
+            user-select: text;
+          }
+          .faq-icon {
+            fill: var(--dl-color-gray-white);
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+          }
+          .faq-element1 {
+            width: 100%;
+            max-width: 800px;
+            padding-top: var(--dl-space-space-threeunits);
+            padding-bottom: var(--dl-space-space-threeunits);
+          }
+          .faq-details1 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            width: 100%;
+            display: flex;
+            align-items: flex-start;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .faq-text02 {
+            color: rgb(255, 255, 255);
+            width: 100%;
+            font-size: 20px;
+            font-style: normal;
+            font-family: Poppins;
+            font-weight: 500;
+            line-height: 28px;
+          }
+          .faq-text03 {
+            color: rgb(204, 204, 204);
+            width: 100%;
+            font-family: Poppins;
+            line-height: 28px;
+            user-select: text;
+          }
+          .faq-icon02 {
+            fill: var(--dl-color-gray-white);
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+          }
+          .faq-element2 {
+            width: 100%;
+            max-width: 800px;
+            padding-top: var(--dl-space-space-threeunits);
+            padding-bottom: var(--dl-space-space-threeunits);
+          }
+          .faq-details2 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            width: 100%;
+            display: flex;
+            align-items: flex-start;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .faq-text04 {
+            color: rgb(255, 255, 255);
+            width: 100%;
+            font-size: 20px;
+            font-style: normal;
+            font-family: Poppins;
+            font-weight: 500;
+            line-height: 28px;
+          }
+          .faq-text05 {
+            color: rgb(204, 204, 204);
+            width: 100%;
+            font-family: Poppins;
+            line-height: 28px;
+            user-select: text;
+          }
+          .faq-icon04 {
+            fill: var(--dl-color-gray-white);
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+          }
+          .faq-element3 {
+            width: 100%;
+            max-width: 800px;
+            padding-top: var(--dl-space-space-threeunits);
+            padding-bottom: var(--dl-space-space-threeunits);
+          }
+          .faq-details3 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            width: 100%;
+            display: flex;
+            align-items: flex-start;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .faq-text06 {
+            color: rgb(255, 255, 255);
+            width: 100%;
+            font-size: 20px;
+            font-style: normal;
+            font-family: Poppins;
+            font-weight: 500;
+            line-height: 28px;
+          }
+          .faq-text07 {
+            color: rgb(204, 204, 204);
+            width: 100%;
+            font-family: Poppins;
+            line-height: 28px;
+            user-select: text;
+          }
+          .faq-icon06 {
+            fill: var(--dl-color-gray-white);
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+          }
+          .faq-element4 {
+            width: 100%;
+            max-width: 800px;
+            padding-top: var(--dl-space-space-threeunits);
+            padding-bottom: var(--dl-space-space-threeunits);
+          }
+          .faq-details4 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            width: 100%;
+            display: flex;
+            align-items: flex-start;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .faq-text08 {
+            color: rgb(255, 255, 255);
+            width: 100%;
+            font-size: 20px;
+            font-style: normal;
+            font-family: Poppins;
+            font-weight: 500;
+            line-height: 28px;
+          }
+          .faq-text09 {
+            color: rgb(204, 204, 204);
+            width: 100%;
+            font-family: Poppins;
+            line-height: 28px;
+            user-select: text;
+          }
+          .faq-icon08 {
+            fill: var(--dl-color-gray-white);
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+          }
+          .faq-element5 {
+            width: 100%;
+            max-width: 800px;
+            padding-top: var(--dl-space-space-threeunits);
+            padding-bottom: var(--dl-space-space-threeunits);
+          }
+          .faq-details5 {
+            gap: var(--dl-space-space-oneandhalfunits);
+            width: 100%;
+            display: flex;
+            align-items: flex-start;
+            flex-direction: column;
+            justify-content: center;
+          }
+          .faq-text10 {
+            color: rgb(255, 255, 255);
+            width: 100%;
+            font-size: 20px;
+            font-style: normal;
+            font-family: Poppins;
+            font-weight: 500;
+            line-height: 28px;
+          }
+          .faq-text11 {
+            color: rgb(204, 204, 204);
+            width: 100%;
+            font-family: Poppins;
+            line-height: 28px;
+            user-select: text;
+          }
+          .faq-icon10 {
+            fill: var(--dl-color-gray-white);
+            width: var(--dl-size-size-xsmall);
+            height: var(--dl-size-size-xsmall);
+          }
+
+          @media (max-width: 767px) {
+            .faq-text {
+              font-size: 16px;
+              line-height: 24px;
+            }
+            .faq-text02 {
+              font-size: 16px;
+              line-height: 24px;
+            }
+            .faq-text04 {
+              font-size: 16px;
+              line-height: 24px;
+            }
+            .faq-text06 {
+              font-size: 16px;
+              line-height: 24px;
+            }
+            .faq-text08 {
+              font-size: 16px;
+              line-height: 24px;
+            }
+            .faq-text10 {
+              font-size: 16px;
+              line-height: 24px;
+            }
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+FAQ.defaultProps = {
+  rootClassName: '',
+}
+
+FAQ.propTypes = {
+  rootClassName: PropTypes.string,
+}
+
+export default FAQ
